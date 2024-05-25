ff82588babc4d555cc5bdb60173b9e0d650800ea
BakhodirovDev <bbahodirov.dev@gmail.com>
2024-05-25 21:07:48 +0500

    first commit

---
+++ Models/DataBase/MainModels/Order/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysHotel.Models.DataBase.MainModels.Order
+{
+    public class Order
+    {
+    }
+}
