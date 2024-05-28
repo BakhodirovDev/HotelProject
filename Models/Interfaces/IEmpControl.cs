b9c6989172a270d9e38af99a2accc0b83d73de5e
sardorbek_hafiz0v <sardorbekhafizov13@gmail.com>
2024-05-28 21:20:37 +0400

    Employe da interface ni qildim.

---
+++ Models/Interfaces/IEmpControl.cs
@@ -0,0 +1,15 @@
+﻿using HotelProject.Models.DataBase.MainModels.EmployeControl;
+using HotelProject.Models.DataBase.MainModels.Login;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.Models.Interfaces
+{
+    public interface IEmpControl
+    {
+        Task<bool> AddUser(EmpControl model);
+    }
+}
---
+++ Models/Interfaces/ISalary.cs
@@ -0,0 +1,14 @@
+﻿using HotelProject.Models.DataBase.MainModels.EmployeControl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.Models.Interfaces
+{
+    public interface ISalary
+    {
+        Task<bool> AddUser(Salary model);
+    }
+}
