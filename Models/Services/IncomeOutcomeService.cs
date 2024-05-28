3238a590c178ee64af66a52761ac2ed2dc2eb139
Mirjahon Istamov <istamovmirjahon8@gmail.com>
2024-05-28 21:16:59 +0500

    connect Service classes and Interface classes

--- Models/Services/IncomeOutcomeService.cs
+++ Models/Services/IncomeOutcomeService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HotelProject.Models.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace HotelProject.Models.Services
 {
-    public class IncomeOutcomeService
+    public class IncomeOutcomeService:IIncomeOutcome
     {
 
     }
