eeb65536c60da7f403d1c86a70156adb5debd852
diyorbekmusayev <laylomusayeve546@gmail.com>
2024-05-28 21:06:43 +0500

    Add OrderClass

--- Models/DataBase/MainModels/Order/Order.cs
+++ Models/DataBase/MainModels/Order/Order.cs
@@ -8,5 +8,12 @@ namespace SysHotel.Models.DataBase.MainModels.Order
 {
     public class Order
     {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ClientId { get; set; }
+        public int RoomId { get; set; }
+        public DateTime EnterTime { get; set; }
+        public DateTime ChickoutTime { get; set; }
+        public double Price { get; set; }
     }
 }
