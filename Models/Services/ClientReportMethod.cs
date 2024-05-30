bd1213224700de260150ff639bd6b73002fb8a00
. <.>
2024-05-30 18:37:02 +0500

    i added all interface

---
+++ Models/Services/ClientReportMethod.cs
@@ -0,0 +1,17 @@
+﻿using HotelProject.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.Models.Services
+{
+    public class ClientReportMethod : IClientReport
+    {
+        public Task<List<ClientReport>> GetClientReport(ClientReport model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
