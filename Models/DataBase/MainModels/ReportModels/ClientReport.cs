5056744efe18b819c0f5b32a0f2c7d3549470c09
. <.>
2024-06-22 22:05:49 +0500

    i added clientReportMethod

---
+++ Models/DataBase/MainModels/ReportModels/ClientReport.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.Models.DataBase.MainModels.ReportModels
+{
+    public class ClientReport
+    {
+        public int id { get; set; }
+        public string FIO { get; set; }
+        public DateTime InCome { get; set; }
+        public DateTime OutCome { get; set; }
+        public string room { get; set; }
+        public string roomType { get; set; }
+        public int LivedTime { get; set; }
+        public int count { get; set; }
+        public double Pay { get; set; }
+    }
+}
--- Models/Services/ClientReportMethod.cs
+++ Models/Services/ClientReportMethod.cs
@@ -1,4 +1,5 @@
-﻿using HotelProject.Models.Interfaces;
+﻿using HotelProject.Models.DataBase.MainModels.ReportModels;
+using HotelProject.Models.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +8,54 @@ using System.Threading.Tasks;
 
 namespace HotelProject.Models.Services
 {
-    public class ClientReportMethod : IClientReport
+    public class ClientReportMethod :IClientReport
     {
-        public Task<List<ClientReport>> GetClientReport(ClientReport model)
+        public Task<List<ClientReport>> clientReportList()
         {
-            throw new NotImplementedException();
+            try
+            {
+                List<ClientReport> clientReports = new List<ClientReport>();
+                return Task.FromResult(clientReports);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
+        public Task<List<ClientReport>> clientSortReport()
+        {
+            try
+            {
+                List<ClientReport> clientReports=new List<ClientReport>();
+                clientReports.Sort();
+                return Task.FromResult(clientReports);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+       public  Task<List<ClientReport>> clientReportFilter(DateTime time1,DateTime time2)
+        {
+            try
+            {
+                List<ClientReport> clientReports = new List<ClientReport>();
+                List<ClientReport> filterClientReport = new List<ClientReport>();
+                foreach (ClientReport clientReport in clientReports)
+                {
+                    if(clientReport.InCome>=time1 && clientReport.OutCome <= time2)
+                    {
+                        filterClientReport.Add(clientReport);
+                    }
+                }
+                return Task.FromResult(filterClientReport); 
+            }
+            catch (Exception ex) 
+            { 
+                throw;
+            }
+        }
+
     }
 }
