ee8bd08566939bf96561c378c31cbddb5bbfdaa4
Mirjahon Istamov <istamovmirjahon8@gmail.com>
2024-06-26 19:37:59 +0500

    activeOrders and search function add

---
+++ Models/Interfaces/IActiveOrders.cs
@@ -0,0 +1,15 @@
+﻿using HotelProject.Models.DataBase.MainModels.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.Models.Interfaces
+{
+    public interface IActiveOrders
+    {
+        Task<List<ActiveOrders>> ActiveOrdersReadAsync();
+        //Task<List<ActiveOrders>> ActiveOrdersSearchAsync(string text);
+    }
+}
---
+++ Models/Services/ActiveOrdersService.cs
@@ -0,0 +1,39 @@
+﻿using HotelProject.Models.DataBase.MainModels.Order;
+using HotelProject.Models.Interfaces;
+using SysHotel.Models.DataBase;
+using SysHotel.Models.DataBase.MainModels.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.Models.Services
+{
+    public class ActiveOrdersService : IActiveOrders
+    {
+        public async Task<List<ActiveOrders>> ActiveOrdersReadAsync()
+        {
+            try
+            {
+                
+                List<ActiveOrders> activeOrder = DbContext.Query<ActiveOrders>("select * from \"OrderData\".\"vwActiveOrders\"");
+                
+                
+                for (int i = 0; i < activeOrder.Count; i++)
+                {
+                    activeOrder[i].TartibRaqam = i + 1;
+                }   
+
+                
+
+                return await Task.FromResult(activeOrder);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+    }
+}
