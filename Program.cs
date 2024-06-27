4b2f6eba6f3fbff438aa38b5751ae52befd2f933
diyorbekmusayev <laylomusayeve546@gmail.com>
2024-06-27 17:02:25 +0500

    program.cs edit

--- Program.cs
+++ Program.cs
@@ -16,11 +16,7 @@ namespace HotelProject
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-<<<<<<< HEAD
-            Application.Run(new AdminForm(1));
-=======
-            Application.Run(new ManagerForm(1));
->>>>>>> a9a5f39b6e6fb50c90146d0bd6bbcc315e17d89f
+            Application.Run(new Login());
         }
     }
 }
